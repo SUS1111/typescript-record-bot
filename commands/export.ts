96fa5325d6325edcf46ea1e5c6b53fed8e0adcfc
SUS1111 <91578804+SUS1111@users.noreply.github.com>
2025-12-17 14:32:05 +0800

    Changed some messages

--- commands/export.ts
+++ commands/export.ts
@@ -9,10 +9,10 @@ export const run = (client: Client, message: Message | ChatInputCommandInteracti
     const user: GuildMember | undefined = memberGet(message, args[0]);
     const connection: VoiceConnection | undefined = getVoiceConnection(message.guildId, client.user.id);
     if(!connection) return reply(message, { content: '機器人尚未加入語音頻道' });
-    if(allRecord.size === 0) return reply(message, { content: '机器人并未开始录音' });
-    if(user && !allRecord.has(user.id)) return reply(message, { content: '机器人并未对于该用户录音' });
+    if(allRecord.size === 0) return reply(message, { content: '机器人并未开始錄音' });
+    if(user && !allRecord.has(user.id)) return reply(message, { content: '机器人并未对于该用户錄音' });
     const stopRecordId: string[] = user ? [user.id] : Array.from(allRecord.keys());
-    return exportRecordAsZip(stopRecordId).then(() => reply(message, { content: '機器人已汇出錄音' }));
+    return exportRecordAsZip(stopRecordId).then(() => reply(message, { content: '機器人已匯出錄音' }));
 };
 
 export const conf: configCommandType = {
@@ -23,5 +23,5 @@ export const conf: configCommandType = {
     args: new Map([
         ['用戶', { required: false, description: '想要导出錄音的用戶 不填則則是导出所有錄音', type: 'user' }],
     ]),
-    description: '停止對語音頻道進行錄音'
+    description: '匯出錄音但仍繼續錄音'
 };
